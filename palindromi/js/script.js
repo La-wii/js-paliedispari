ebc31e6d51eabad50a876f5564db0f63338ccb8d
La-wii <sentilavale@gmail.com>
2021-06-07 18:37:34 +0200

    funzione palindroma

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -15,12 +15,25 @@ console.log(parolaInvertita);
 
 // senza funzione si può fare così
 
-if(parolaUtente == parolaInvertita){
+// if(parolaUtente == parolaInvertita){
+//     console.log('la parola è palindroma');
+// } else {
+//     console.log('la parola non è palindroma');
+// }
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+var parolaInversa = invertiParola(parolaUtente); 
+
+if(parolaUtente == parolaInversa){
     console.log('la parola è palindroma');
 } else {
     console.log('la parola non è palindroma');
 }
-
-// Creare una funzione per capire se la parola inserita è palindroma
+  
+function invertiParola(str){
+  var strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
 
 
