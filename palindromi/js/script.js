52b42db1aab9f45a8dcd8ebde6362f53c8175f6c
La-wii <sentilavale@gmail.com>
2021-06-07 18:32:15 +0200

    palindromo senza funzione

---
+++ palindromi/js/script.js
@@ -0,0 +1,26 @@
+
+// Chiedere all’utente di inserire una parola. 
+
+var parolaUtente = prompt('Inserisci una parola');
+console.log(parolaUtente);  
+
+var parolaDivisa = parolaUtente.split('');
+console.log(parolaDivisa);
+
+var parolaDivisa = parolaDivisa.reverse();
+console.log(parolaDivisa);
+
+var parolaInvertita = parolaDivisa.join('');
+console.log(parolaInvertita);
+
+// senza funzione si può fare così
+
+if(parolaUtente == parolaInvertita){
+    console.log('la parola è palindroma');
+} else {
+    console.log('la parola non è palindroma');
+}
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
